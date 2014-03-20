1bf6eeaf1b7529cf505edc4b710e12362d32a96e
widmogrod <widmogrod@gmail.com>
2014-03-20 22:59:36 +0100

    import models

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 require.config({
     baseUrl: "js",
     paths: {
-        three: 'vendor/three.js/three.min'
+        three: 'vendor/three.js/three.min',
+        text: 'vendor/requirejs-text/text',
+        json: 'vendor/requirejs-plugins/src/json'
     },
     shim: {
         three: {
@@ -10,7 +12,7 @@ require.config({
     }
 });
 
-require(['three'], function(THREE) {
+require(['three', 'json!model.js', 'json!sus.js'], function(THREE, model, sus) {
     'use strict';
 
     var scene = new THREE.Scene();
@@ -27,11 +29,25 @@ require(['three'], function(THREE) {
 
     camera.position.z = 5;
 
+    var loader = new THREE.JSONLoader();
+    var myModel = loader.parse(model);
+    var cube2 = new THREE.Mesh(myModel.geometry, myModel.material);
+    cube2.position.x = -2;
+    scene.add(cube2);
+
+    var myModel = loader.parse(sus);
+    var sus = new THREE.Mesh(myModel.geometry, myModel.material);
+    sus.position.x = 2
+    scene.add(sus);
+
+
     var render = function () {
         requestAnimationFrame(render);
 
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        sus.rotation.y += 0.01;
 
         renderer.render(scene, camera);
     };
